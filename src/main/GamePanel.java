da2bb7530d264dad66fe3b3ec876d8d38ca107fc
Aiden Egglin <uefadude@gmail.com>
2025-04-10 19:22:35 -0400

    Abstract player functionality

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import java.lang.Thread;
 
 import java.awt.Color;
@@ -14,7 +16,7 @@ public class GamePanel extends JPanel implements Runnable {
     private final int originalTileSize = 16; // 16 x 16 tile
     private final int scale = 3; // used for scaling for higher resolution monitors
 
-    private final int tileSize = originalTileSize * scale; // 48 x 48 tile
+    public final int tileSize = originalTileSize * scale; // 48 x 48 tile
     private final int maxScreenCol = 16;
     private final int maxScreenRow = 12;
     private final int screenWidth = tileSize * maxScreenCol; // 768 pixels
@@ -25,9 +27,7 @@ public class GamePanel extends JPanel implements Runnable {
     private KeyHandler keyHandler = new KeyHandler();
     private Thread gameThread; // Can start and stop
 
-    private int playerX = 100;
-    private int playerY = 100;
-    private int playerSpeed = 4;
+    Player player = new Player(this, keyHandler);
 
     public GamePanel() {
         // JPanel methods
@@ -96,22 +96,16 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        if (keyHandler.upPressed)
-            playerY -= playerSpeed;
-        else if (keyHandler.downPressed)
-            playerY += playerSpeed;
-        else if (keyHandler.leftPressed)
-            playerX -= playerSpeed;
-        else if (keyHandler.rightPressed)
-            playerX += playerSpeed;
+       player.update();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
+
+   
         g2.dispose(); // Not strictly necessary
     }
 }
