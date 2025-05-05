e2b1ace0e81e9b641630159ac3b71aead658184e
Aiden Egglin <uefadude@gmail.com>
2025-05-04 20:26:47 -0400

    Finish moving getClass() to File() calls

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -51,27 +51,25 @@ public class TileManager {
     public void getTileImages() {
         try {
             tile[0] = new Tile();
-            File grassFile = new File("2DGame/res/tiles/grass.png");
-            System.out.println(grassFile.getAbsolutePath());
-            tile[0].image = ImageIO.read(grassFile);
+            tile[0].image = ImageIO.read(new File("2DGame/res/tiles/grass.png"));
 
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(new File("2DGame/res/player/wall.png"));
+            tile[1].image = ImageIO.read(new File("2DGame/res/tiles/wall.png"));
             tile[1].collision = true;
 
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(new File("2DGame/res/player/water.png"));
+            tile[2].image = ImageIO.read(new File("2DGame/res/tiles/water.png"));
             tile[2].collision = true;
 
             tile[3] = new Tile();
-            tile[3].image = ImageIO.read(new File("2DGame/res/player/earth.png"));
+            tile[3].image = ImageIO.read(new File("2DGame/res/tiles/earth.png"));
 
             tile[4] = new Tile();
-            tile[4].image = ImageIO.read(new File("2DGame/res/player/tree.png"));
+            tile[4].image = ImageIO.read(new File("2DGame/res/tiles/tree.png"));
             tile[4].collision = true;
 
             tile[5] = new Tile();
-            tile[5].image = ImageIO.read(new File("2DGame/res/player/sand.png"));
+            tile[5].image = ImageIO.read(new File("2DGame/res/tiles/sand.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
